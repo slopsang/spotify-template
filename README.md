5226073859b8c54f2c2e3f2e9e7bc42f10b126b1
Sang Ouk Kim <slopsang1@gmail.com>
2015-11-09 02:12:52 -0800

    pretty much done

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # spotify-template
 This template is an optional starting point for a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/spotify).
+
+What is the purpose of your project (i.e., why would someone use this app)?
+- The purpose of this project is to allow users to find their favorite songs, artists, or albums interactively.
+Users also can see related songs, artists, or albums while searching so it is very useful to found a new songs.
+
+What is the URL of this project on your student web-server?
+- http://students.washington.edu/sangkim1/info343/spotify-template/
+
+Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+- I worked with classmate (Julian, Katrina) and shared ideas.
+
+Approximately how many hours did it take you to complete this challenge?
+- 10-20 hrs.
+
+Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+- It was hard to understand how spotify API works at first.
--- app.js
+++ app.js
@@ -1,15 +1,81 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var songUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var albumUrl = 'https://api.spotify.com/v1/search?type=album&query='
+var artistUrl = 'https://api.spotify.com/v1/search?type=artist&query='
+var artistSearchUrl = 'https://api.spotify.com/v1/artists/'
+var albumSearchUrl = 'https://api.spotify.com/v1/albums/'
+var songSearchUrl ='https://api.spotify.com/v1/tracks/'
+
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
+  $scope.audioObject = {};
+  // function that allows user to find artists based on user input
+  $scope.getArtists = function() {
+    $scope.findAlbum = null;
+    $scope.findTrack = null;
+    $http.get(artistUrl + $scope.findArtist).success(function(response){
+      data = $scope.artists = response.artists.items;
+    });
+  };
+  // function that allows user to find songs based on user input
   $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
-    })
+    $scope.findArtist = null;
+    $scope.findAlbum = null;
+    $http.get(songUrl + $scope.findTrack).success(function(response){
+      data = $scope.tracks = response.tracks.items;
+    });
+  };
+  // function that allows user to find albums based on user input
+  $scope.getAlbums = function() {    
+    $scope.findArtist = null;
+    $scope.findTrack = null;
+    $http.get(albumUrl + $scope.findAlbum).success(function(response){
+      data = $scope.albums = response.albums.items;
+    });
+  };
+  // function that allows user to find related artists and top-tracks by click on album covers
+  $scope.relatedArtist = function(artist) {
+    $scope.albums = [];
+    $scope.songs = [];
+    $http.get(artistSearchUrl + artist.id + '/albums').success(function(response){
+        data = $scope.albums = response.items;
+    });
+    $http.get(artistSearchUrl + artist.id + '/top-tracks').success(function(response){
+        data = $scope.tracks = response.tracks;
+    });
+  };
+  // function that allows user to see related artists by click on albums or tracks
+  function findTheArtists(artists) {
+    artists.forEach(function(artist) {
+        $http.get(artistSearchUrl + artist.id).success(function(response){
+            data = $scope.artists.push(response);
+        });
+    });
   }
+  // function that allows user to see related artists and tracks by click on album covers
+  $scope.relatedAlbum = function(album) {
+    $scope.artists = [];
+    $scope.tracks = [];
+    $http.get(albumSearchUrl + album.id).success(function(response){
+        findTheArtists(response.artists);
+        response.tracks.items.forEach(function(track) {
+            $http.get(songSearchUrl + track.id).success(function(response){
+                data = $scope.tracks.push(response);
+            });
+        });
+    });
+  };
+  // function that allows user to see related albums and artists by click on songs
+  $scope.relatedSong = function(track) {
+    $scope.artists = [];
+    $scope.albums = [];
+    $http.get(songSearchUrl + track.id).success(function(response){
+        findTheArtists(response.artists);
+        data = $scope.albums = [response.album];
+    });
+  };
+  // function that allows user to listen preview of certain song
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -23,9 +89,8 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
-})
-
+});
 // Add tool tips to anything with a title property
 $('body').tooltip({
-    selector: '[title]'
+  selector: '[title]'
 });
